8774994c0fad2ff386fdc775d13c3964139b6ee4
Knock <enoch10evelyn@gmail.com>
2022-07-19 17:43:24 -0700

    HTML,CSS and start of JS created

---
+++ assets/script.js
@@ -0,0 +1,10 @@
+//Capturing HTML elements to manipulate via DOM
+
+var startButton = document.querySelector("#start-btn")
+
+document.addEventListener("click", startGame);
+
+function startGame() {
+    console.log("Started");
+    
+}
